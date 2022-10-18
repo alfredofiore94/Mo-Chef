84d88ff43b666d6a72d3ab48219a69c2ce075d6f
Alfredo Fiore <alfredofiore@Air-Moveo.homenet.telecomitalia.it>
2022-10-18 10:03:53 +0200

    primo commit

---
+++ Mo-Chef/Mo_ChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mo_ChefApp.swift
+//  Mo-Chef
+//
+//  Created by Alfredo Fiore on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Mo_ChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

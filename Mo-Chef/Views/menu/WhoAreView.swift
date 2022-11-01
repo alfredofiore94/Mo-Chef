1ceec5d3f1030aa831e5a58265372872391987e6
Alfredo Fiore <alfredofiore@live.com>
2022-11-01 20:25:16 +0100

    social links

--- Mo-Chef/Views/menu/WhoAreView.swift
+++ Mo-Chef/Views/menu/WhoAreView.swift
@@ -15,6 +15,9 @@ struct WhoAreView: View {
             ScrollView{
                 VStack{
                     moveoBtn
+                    socialBtn(nome: "Linkedin")
+                    socialBtn(nome: "Twitter")
+                    socialBtn(nome: "Facebook")
                 }
             }
         }
@@ -32,15 +35,12 @@ extension WhoAreView{
                     .stroke(Mo_ChefApp.verdeScuro, lineWidth: 5)
                     .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
                 
-                
                 Image("logo-white-moveo")
                     .renderingMode(.template)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .foregroundColor(.red)
                     .padding(5)
-                
-                
             }
             .frame(width: .infinity, height: 50)
             .padding(.trailing, 30)
@@ -48,6 +48,55 @@ extension WhoAreView{
             .padding(.bottom, 0)
             .padding(.top, 30)
             .foregroundColor(Mo_ChefApp.arancioneCosmo)
+            .font(.custom("SF Pro Text", size: 25))
+        }
+    }
+    
+    private func socialBtn(nome: String) -> some View{
+        
+        let nomeLower = nome.lowercased()
+        var link : String
+        switch nomeLower {
+        case "linkedin":
+            link = "https://www.linkedin.com/company/moveo-srl/mycompany/"
+        case "twitter":
+            link = "https://twitter.com/MoveoConsulting/"
+        case "facebook":
+            link = "https://www.facebook.com/moveoconsulting"
+        default:
+            link = ""
+        }
+        
+        return Link(destination: URL(string: link)!){
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Mo_ChefApp.verdeScuro, lineWidth: 5)
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
+                HStack{
+                    Image(nomeLower+"_icon")
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.red)
+                        .padding(5)
+                        .padding(.leading, 25)
+                        .frame(width: 80, alignment: .leading)
+                    Spacer()
+
+                    Text(nome)
+                        .frame(alignment: .leading)
+                    Spacer()
+                    Spacer()
+                }
+                
+            }
+            .frame(width: .infinity, height: 50)
+            .padding(.trailing, 30)
+            .padding(.leading, 30)
+            .padding(.bottom, 0)
+            .padding(.top, 30)
+            .foregroundColor(Mo_ChefApp.verdeScuro)
             .font(.custom("SF Pro Text", size: 25))
         }
     }

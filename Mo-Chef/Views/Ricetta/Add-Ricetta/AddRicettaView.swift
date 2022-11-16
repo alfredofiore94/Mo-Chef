6681c4e4fb489193e11675c24112d5230a8108b0
alfredofiore94 <90612556+alfredofiore94@users.noreply.github.com>
2022-11-16 17:57:25 +0100

    struttura iniziale add ricetta

---
+++ Mo-Chef/Views/Ricetta/Add-Ricetta/AddRicettaView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRicettaView.swift
+//  Mo-Chef
+//
+//  Created by Alfredo Fiore on 16/11/22.
+//
+
+import SwiftUI
+
+struct AddRicettaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRicettaView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRicettaView()
+    }
+}

62e0db5ba4473d36d38177e3aaf7c0b9f04e3ece
alfredofiore94 <90612556+alfredofiore94@users.noreply.github.com>
2023-01-24 14:18:51 +0100

    fix dati: creazione datamanager per centralizzare i dati

--- Mo-Chef/Mo_ChefApp.swift
+++ Mo-Chef/Mo_ChefApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct Mo_ChefApp: App {
     
+    @StateObject var listaRicette: ListaRicettaModel = ListaRicettaModel()
+    
     static let arancioneCosmo: Color = Color(red: 239 / 255, green: 128 / 255, blue: 74 / 255)
     
     static let verdeScuro: Color = Color(red: 58 / 255, green: 173 / 255, blue: 168 / 255)
@@ -21,30 +23,7 @@ struct Mo_ChefApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView().ignoresSafeArea()
+                .environmentObject(listaRicette)
         }
-    }
-}
-
-struct DataTest {
-    static func getDataTest() -> [Ricetta]{
-        var ricettaLista: [Ricetta] = []
-        
-        var listaIngr: [Ingrediente] = [Ingrediente(nome: "zucchero", quantita: 23.5, uMisura: "g", isSelezionato: false)]
-        let presentazione = "Piccolo testo di presentazione del piatto. Si può descrivere la storia del piatto o altre introduzioni, come ‘un piatto simbolo dell’Italia"
-        let count = 1...10
-        for _ in count { listaIngr.append(Ingrediente(nome: "farina", quantita: 23.5, uMisura: "g", isSelezionato: false))
-        }
-        
-        let listaStep: [StepProcedimento] = [
-            StepProcedimento(nome: "Lievitino", descrizione: "Prima di tutto preparate l'emulsione aromatica che è ciò determina sapore e profumo al pandoro, ecco perchè questo passaggio non andrebbe mai trascurato o sottovalutato.", immagini: ["pizza", "pizza"]),
-            StepProcedimento(nome: "Lievitino", descrizione: "Prima di tutto preparate l'emulsione aromatica che è ciò determina sapore e profumo al pandoro, ecco perchè questo passaggio non andrebbe mai trascurato o sottovalutato.", immagini: ["pizza"])
-        ]
-        
-        ricettaLista.append(Ricetta(nomePiatto: "Pasta", tipopiatto: "primo", difficolta: "facile", tempoPrep: "30 ore", costo: "economico", numeroPersone: 5, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
-        ricettaLista.append(Ricetta(nomePiatto: "Carne", tipopiatto: "secondo", difficolta: "difficile", tempoPrep: "12 ore", costo: "costoso", numeroPersone: 3, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
-        ricettaLista.append(Ricetta(nomePiatto: "Cavatelli al sugo di cinghiale", tipopiatto: "secondo", difficolta: "difficile", tempoPrep: "12 ore", costo: "costoso", numeroPersone: 3, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
-        
-        return ricettaLista
-        
     }
 }
---
+++ Mo-Chef/Model/DataManager.swift
@@ -0,0 +1,61 @@
+//
+//  DataManager.swift
+//  Mo-Chef
+//
+//  Created by Alfredo Fiore on 23/01/23.
+//
+
+import Foundation
+
+class ListaRicettaModel: ObservableObject {
+        
+    @Published var items: [Ricetta] = []
+    
+    let DEV_MODE_ENABLED = true
+    
+    init() {
+        getListaRicette()
+    }
+
+    
+    func getListaRicette() -> [Ricetta] {
+        
+        if(DEV_MODE_ENABLED){
+            items.append(contentsOf: DataTest.getDataTest())
+        }
+        
+        return items
+    }
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveltem(from: IndexSet, to: Int) {
+        items.move (fromOffsets: from, toOffset: to)
+    }
+}
+
+
+struct DataTest {
+    static func getDataTest() -> [Ricetta]{
+        var ricettaLista: [Ricetta] = []
+        
+        var listaIngr: [Ingrediente] = [Ingrediente(nome: "zucchero", quantita: 23.5, uMisura: "g", isSelezionato: false)]
+        let presentazione = "Piccolo testo di presentazione del piatto. Si può descrivere la storia del piatto o altre introduzioni, come ‘un piatto simbolo dell’Italia"
+        let count = 1...10
+        for _ in count { listaIngr.append(Ingrediente(nome: "farina", quantita: 23.5, uMisura: "g", isSelezionato: false))
+        }
+        
+        let listaStep: [StepProcedimento] = [
+            StepProcedimento(nome: "Lievitino", descrizione: "Prima di tutto preparate l'emulsione aromatica che è ciò determina sapore e profumo al pandoro, ecco perchè questo passaggio non andrebbe mai trascurato o sottovalutato.", immagini: ["pizza", "pizza"]),
+            StepProcedimento(nome: "Lievitino", descrizione: "Prima di tutto preparate l'emulsione aromatica che è ciò determina sapore e profumo al pandoro, ecco perchè questo passaggio non andrebbe mai trascurato o sottovalutato.", immagini: ["pizza"])
+        ]
+        
+        ricettaLista.append(Ricetta(nomePiatto: "Pasta", tipopiatto: "primo", difficolta: "facile", tempoPrep: "30 ore", costo: "economico", numeroPersone: 5, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
+        ricettaLista.append(Ricetta(nomePiatto: "Carne", tipopiatto: "secondo", difficolta: "difficile", tempoPrep: "12 ore", costo: "costoso", numeroPersone: 3, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
+        ricettaLista.append(Ricetta(nomePiatto: "Cavatelli al sugo di cinghiale", tipopiatto: "secondo", difficolta: "difficile", tempoPrep: "12 ore", costo: "costoso", numeroPersone: 3, listaIngredinti: listaIngr, listastep: listaStep, presentazione: presentazione))
+        
+        return ricettaLista
+        
+    }
+}
